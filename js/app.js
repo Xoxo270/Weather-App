b887bb676b81cbd5994af4b4c4c322a4a2425dd2
Xoxo270 <47757884+Xoxo270@users.noreply.github.com>
2023-10-18 19:18:45 +0200

    Final upload

---
+++ js/app.js
@@ -0,0 +1,70 @@
+"use strict";
+
+//  Variables
+let cityName = document.getElementById("cityName");
+let cityTemp = document.getElementById("cityTemp");
+let weatherLogo = document.getElementById("weatherLogo");
+let weatherText = document.getElementById("weatherText");
+let searchField = document.getElementById("searchField");
+let searchBtn = document.getElementById("searchBtn");
+
+// Weather for your navigator's location on load
+window.addEventListener("load", () => {
+  let geolocUrl = `https://api.weatherapi.com/v1/current.json?key=8685490f1f8f4420b26124327231204&q=Paris&aqi=no`;
+  fetchWeather(geolocUrl);
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let [lat, long] = [position.coords.latitude, position.coords.longitude];
+      let geolocUrl = `https://api.weatherapi.com/v1/current.json?key=8685490f1f8f4420b26124327231204&q=${lat},${long}&aqi=no`;
+      fetchWeather(geolocUrl);
+    });
+  }
+});
+// Listening to the button event to get the search field value
+searchBtn.onclick = (event) => {
+  event.preventdefault;
+  let url = `https://api.weatherapi.com/v1/current.json?key=8685490f1f8f4420b26124327231204&q=${searchField.value}&aqi=no`;
+  searchField.value = "";
+  fetchWeather(url);
+};
+// Listening to keydown event for 'Enter' in the search field
+searchField.onkeydown = (event) => {
+  if (event.key === "Enter") {
+    let url = `https://api.weatherapi.com/v1/current.json?key=8685490f1f8f4420b26124327231204&q=${searchField.value}&aqi=no`;
+    searchField.value = "";
+    fetchWeather(url);
+  }
+};
+
+// Async function
+const fetchWeather = async (url) => {
+  try {
+    await fetch(url)
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      // Render the data
+      .then((data) => {
+        clear();
+        cityName.append(`${data.location.name}`);
+        cityTemp.append(`${data.current.temp_c}°C / ${data.current.temp_f}°F`);
+        weatherLogo.src = `${data.current.condition.icon}`;
+        weatherText.append(`${data.current.condition.text}`);
+      });
+  } catch (error) {
+    console.error(`Error message : ${error.message}`);
+    cityName.append(
+      `This city name does not exist , please search another one.`
+    );
+  }
+};
+
+// Simple clear function to empty all fields
+const clear = () => {
+  cityName.innerHTML = "";
+  cityTemp.innerHTML = "";
+  weatherLogo.innerHTML = "";
+  weatherText.innerHTML = "";
+  searchField.innerHTML = "";
+};
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Weather Application
+
+A simple application that let you know the current weather in a city of your choice.
+
+## Install
+
+```
+npm install
+npm run start
+```
+
+Open `http://localhost:9000` in a browser tab
+
+## TODOs
+
+- [x] Read weather API documentation
+- [x] Prepare the HTML structure
+- [x] Call the weather API for the first time to get a result
+- [x] Render some data from the API (city name , temperature , logo)
+- [x] Add a searchbox that takes a city name
+- [x] Add load event function that renders the weather for your location
+- [x] Add a clear function that empties all fields
+- [x] Add an else to handle wrong city names
+- [x] Add some CSS to embelish the whole application
